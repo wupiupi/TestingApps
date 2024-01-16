bff2c7de668c488d965ec35c26311bcd7020955b
Paul <pasha.makey@gmail.com>
2024-01-16 18:58:43 +0300

    🔧 chore: done with settingsVC

--- TestingApps/ViewControllers/SettingsViewController.swift
+++ TestingApps/ViewControllers/SettingsViewController.swift
@@ -9,9 +9,21 @@ import UIKit
 
 final class SettingsViewController: UIViewController {
     
+    @IBOutlet var minValueTF: UITextField!
+    @IBOutlet var maxValueTF: UITextField!
+    
+    var minVal: String!
+    var maxVal: String!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        minValueTF.text = minVal
+        maxValueTF.text = maxVal
     }
     
+    @IBAction func closeButtonAction() {
+        dismiss(animated: true)
+    }
     
 }
