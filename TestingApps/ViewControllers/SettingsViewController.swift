8cafdee3b454cf7fd8111fca9538798db2a606f7
Paul <pasha.makey@gmail.com>
2024-01-16 17:58:44 +0300

    ✨ project: added new settingsVC class

---
+++ TestingApps/ViewControllers/SettingsViewController.swift
@@ -0,0 +1,17 @@
+//
+//  SettingsViewController.swift
+//  TestingApps
+//
+//  Created by Paul Makey on 16.01.24.
+//
+
+import UIKit
+
+final class SettingsViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    
+}
