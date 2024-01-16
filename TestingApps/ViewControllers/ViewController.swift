faebaec86be0c29fcde5d84b9f4819ab8dd43342
Paul <pasha.makey@gmail.com>
2024-01-16 19:20:04 +0300

    🔧 chore: removed unnecessary code

--- TestingApps/ViewControllers/ViewController.swift
+++ TestingApps/ViewControllers/ViewController.swift
@@ -7,12 +7,7 @@ final class ViewController: UIViewController {
     @IBOutlet var maximumValueLabel: UILabel!
     
     @IBOutlet var resultValueLabel: UILabel!
-    
-    // MARK: - Life Cycle
-    override func viewDidLoad() {
-        super.viewDidLoad()
-    }
-    
+ 
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         let settingsVC = segue.destination as? SettingsViewController
         settingsVC?.maxVal = maximumValueLabel.text
@@ -32,4 +27,24 @@ final class ViewController: UIViewController {
         minimumValueLabel.text = settingsVC?.minValueTF.text
     }
     
+}
+
+private extension ViewController {
+    func showAlert() {
+        let alert = UIAlertController(
+            title: "Error",
+            message: "min cannot be more than max",
+            preferredStyle: .alert
+        )
+
+        let action = UIAlertAction(
+            title: "Ok",
+            style: .default) { _ in
+                self.minimumValueLabel.text = "0"
+                self.maximumValueLabel.text = "100"
+            }
+        
+        alert.addAction(action)
+        present(alert, animated: true)
+    }
 }
