849d4e9751629d1290dcd6f947aa3114b32259d6
Paul <pasha.makey@gmail.com>
2024-01-16 16:05:05 +0300

    Initial Commit

---
+++ TestingApps/ViewControllers/MainViewController.swift
@@ -0,0 +1,20 @@
+import UIKit
+
+final class ViewController: UIViewController {
+    
+    // MARK: - IB Outlets
+    @IBOutlet var minimumValueLabel: UILabel!
+    @IBOutlet var maximumValueLabel: UILabel!
+    
+    @IBOutlet var resultValueLabel: UILabel!
+    
+    // MARK: - Life Cycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    
+    @IBAction func generateButtonAction() {
+    }
+    
+}
