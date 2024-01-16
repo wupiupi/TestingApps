03181954989bd5376ed9af00a6d78e814f4fad56
Paul <pasha.makey@gmail.com>
2024-01-16 23:13:09 +0300

    Initial setup of new project: "About me"

---
+++ TestingApps/ViewControllers/AboutViewController.swift
@@ -0,0 +1,12 @@
+//
+//  AboutViewController.swift
+//  TestingApps
+//
+//  Created by Paul Makey on 16.01.24.
+//
+
+import UIKit
+
+final class AboutViewController: UIViewController {
+
+}
--- TestingApps/ViewControllers/ViewController.swift
+++ TestingApps/ViewControllers/ViewController.swift
@@ -1,55 +1,9 @@
 import UIKit
 
-final class ViewController: UIViewController {
+final class LoginViewController: UIViewController {
     
-    // MARK: - IB Outlets
-    @IBOutlet var minimumValueLabel: UILabel!
-    @IBOutlet var maximumValueLabel: UILabel!
-    
-    @IBOutlet var resultValueLabel: UILabel!
- 
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        let settingsVC = segue.destination as? SettingsViewController
-        settingsVC?.maxVal = maximumValueLabel.text
-        settingsVC?.minVal = minimumValueLabel.text
+    override func viewDidLoad() {
+        super.viewDidLoad()
     }
     
-    @IBAction func generateButtonAction() {
-        let minVal = Int(minimumValueLabel.text ?? "") ?? 0
-        let maxVal = Int(maximumValueLabel.text ?? "") ?? 100
-        
-        if minVal > maxVal {
-            showAlert()
-            return
-        }
-        
-        resultValueLabel.text = Int.random(in: minVal...maxVal).formatted()
-    }
-    
-    @IBAction func unwind(for segue: UIStoryboardSegue) {
-        let settingsVC = segue.source as? SettingsViewController
-        maximumValueLabel.text = settingsVC?.maxValueTF.text
-        minimumValueLabel.text = settingsVC?.minValueTF.text
-    }
-    
-}
-
-private extension ViewController {
-    func showAlert() {
-        let alert = UIAlertController(
-            title: "Error",
-            message: "min cannot be more than max",
-            preferredStyle: .alert
-        )
-
-        let action = UIAlertAction(
-            title: "Ok",
-            style: .default) { _ in
-                self.minimumValueLabel.text = "0"
-                self.maximumValueLabel.text = "100"
-            }
-        
-        alert.addAction(action)
-        present(alert, animated: true)
-    }
 }
