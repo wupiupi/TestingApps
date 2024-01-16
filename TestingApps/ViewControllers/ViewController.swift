48635332d1291f9594522165c9f041ea4c8fab86
Paul <pasha.makey@gmail.com>
2024-01-16 23:28:38 +0300

    🔧 chore: alerts + data verification

--- TestingApps/ViewControllers/ViewController.swift
+++ TestingApps/ViewControllers/ViewController.swift
@@ -2,8 +2,42 @@ import UIKit
 
 final class LoginViewController: UIViewController {
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
+    @IBOutlet var usernameTextField: UITextField!
+    @IBOutlet var passwordTextField: UITextField!
+    
+    private let username = "Paul"
+    private let password = "123"
+    
+    @IBAction func loginButtonDidTapped() {
+        if !(usernameTextField.text == username && passwordTextField.text == password) {
+            showAlert(title: "Error!", message: "Login or password is invalid")
+        }
     }
     
+    @IBAction func forgotUsernameButtonDIdTapped() {
+        showAlert(title: "", message: "Your username is: \(username)")
+    }
+    
+    
+    @IBAction func forgotPasswordButtonDidTapped() {
+        showAlert(title: "", message: "Your password is: \(password)")
+    }
+}
+
+private extension LoginViewController {
+    func showAlert(title: String, message: String) {
+        let alert = UIAlertController(
+            title: title,
+            message: message,
+            preferredStyle: .alert
+        )
+        
+        let action = UIAlertAction(title: "Ok", style: .default) { _ in
+            self.passwordTextField.text = nil
+        }
+        
+        alert.addAction(action)
+        
+        present(alert, animated: true)
+    }
 }
