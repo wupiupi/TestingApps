0db5bb5b088bc9f46cce983eee54df52bad144a3
Paul <pasha.makey@gmail.com>
2024-01-17 17:53:07 +0300

    💄 style: gradient background

--- TestingApps/ViewControllers/AboutViewController.swift
+++ TestingApps/ViewControllers/AboutViewController.swift
@@ -35,6 +35,16 @@ final class AboutViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        let gradientLayer = CAGradientLayer()
+        gradientLayer.frame = view.bounds
+        gradientLayer.colors = [
+            UIColor.systemPink.cgColor,
+            UIColor.cyan.cgColor
+        ]
+        
+        view.layer.insertSublayer(gradientLayer, at: 0)
+        
+        
         let randomGreeting = greetings.randomElement() ?? "Hello!"
         greetingLabel.text = randomGreeting + " " + username
     }
