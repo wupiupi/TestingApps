16639b7e07dd45716053ebf7650c3a95f485875c
Paul <pasha.makey@gmail.com>
2024-01-17 18:03:57 +0300

    💄 style: minor improvements

--- TestingApps/ViewControllers/ViewController.swift
+++ TestingApps/ViewControllers/ViewController.swift
@@ -48,18 +48,12 @@ final class LoginViewController: UIViewController {
 
 private extension LoginViewController {
     func showAlert(title: String, message: String, completion: (() -> Void)? = nil) {
-        let alert = UIAlertController(
-            title: title,
-            message: message,
-            preferredStyle: .alert
-        )
-        
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
         let action = UIAlertAction(title: "Ok", style: .default) { _ in
             completion?()
         }
         
         alert.addAction(action)
-        
         present(alert, animated: true)
     }
 }
