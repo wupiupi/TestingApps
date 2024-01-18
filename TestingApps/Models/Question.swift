992b5b81bbbc223188e51fc93cae15e791383863
Paul <pasha.makey@gmail.com>
2024-01-18 12:15:57 +0300

    🔧 chore: initial model setup

--- TestingApps/Models/Question.swift
+++ TestingApps/Models/Question.swift
@@ -5,3 +5,40 @@
 //  Created by Paul Makey on 18.01.24.
 //
 
+struct Question {
+    let title: String
+    let type: QuestionType
+    let answers: [Answer]
+    
+}
+
+enum QuestionType {
+    case single
+    case multiple
+    case ranged
+}
+
+struct Answer {
+    let title: String
+    let animal: Animal
+}
+
+enum Animal: Character {
+    case dog = "🐶"
+    case cat = "😼"
+    case rabbit = "🐰"
+    case turtle = "🐢"
+    
+    var description: String {
+        switch self {
+            case .dog:
+                "You like to be with your friends. You're very reliable person"
+            case .cat:
+                "You like to make everything by urself and be alone"
+            case .rabbit:
+                "You like everything to be soft. You're healthy and funny person"
+            case .turtle:
+                "Turtle. You're turtle man. Like Raphael or Leonardo (i guess)"
+        }
+    }
+}
